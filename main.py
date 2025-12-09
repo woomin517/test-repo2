064aac3245d917943195d3dcaa447991d81f11c0
이우민 <daegel517@gmail.com>
2025-12-09 14:08:27 +0900

    Add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+
+# 두수 입력받기
+first = input("Enter the first number: ")
+second = input("Enter the second number: ")
+
+# 정수로 변환
+num1 = int(first)
+num2 = int(second)
+
+# 합 계산
+result = num1 + num2
+
+#결과 출력
+print("Sum:", result) 
